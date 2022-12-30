5fb77f8929a451aa9d614347becee144cd6f90ea
ckswls147@naver.com <ckswls147@naver.com>
2022-12-30 16:35:44 +0900

    Initial commit

---
+++ src/main/java/proj/dev_proj/DevProjApplication.java
@@ -0,0 +1,13 @@
+package proj.dev_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/dev_proj/DevProjApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.dev_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
